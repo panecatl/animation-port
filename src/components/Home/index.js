57ba5d2be51e80dd6d0c46f46e19f30d979c251c
panecatl <ypanecatl24@gmail.com>
2023-02-03 11:22:14 -0600

    compleated about me section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo'
 import './index.scss';
-import Logo from './Logo';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
